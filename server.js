7328d18ce0600ce70e7f897aa5e6db8facb68b4c
eli fridman <thisiselimail@gmail.com>
2017-05-19 22:36:54 +0300

    update

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.port||3000
+const PORT = process.env.PORT || 3000;
 
-app.use(function(req,res,next){
-  if(req.headers['x-forwarded-proto']==='https'){
-    res.redirect('http://' + req.hostname + req.url );
-  }else{
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
     next();
   }
 });
@@ -15,5 +15,5 @@ app.use(function(req,res,next){
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port '+PORT);
+  console.log('Express server is up on port ' + PORT);
 });
