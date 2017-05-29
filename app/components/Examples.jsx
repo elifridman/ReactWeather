d70080e5f3d21b7d62d6bbd82d6cfcfc9c9c7fff
eli fridman <thisiselimail@gmail.com>
2017-05-29 21:30:00 +0300

    update example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function () {
@@ -16,7 +17,18 @@ var React = require('react');
 
 var Examples = (props) =>{
   return (
-    <h3>Examples Component</h3>
+    <div >
+      <h1 className="text-center">Examples</h1>
+      <p>check our locations examples</p>
+      <ol>
+        <li>
+             <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+           <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
   )
 };
 
